9d450512cf56b53c0a85c459657f0b7e6d43e774
Ricardo Vale <ricardo_vale@live.com.pt>
2025-03-23 20:52:29 +0000

    Update __init__.py
    Signed-off-by: Ricardo Vale <ricardo_vale@live.com.pt>

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -6,6 +6,8 @@ from .interaction import router as interaction_router
 from .simulation import router as simulation_router
 from .forces import router as forces_router
 from .absolute import router as absolute_router
+from .visualization import router as visualization_router
+
 api_router = APIRouter()
 
 # 🔗 Attach all module routes
@@ -13,3 +15,4 @@ api_router.include_router(interaction_router)
 api_router.include_router(simulation_router)
 api_router.include_router(forces_router)
 api_router.include_router(absolute_router)
+api_router.include_router(visualization_router)
