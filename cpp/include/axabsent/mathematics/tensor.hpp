c92c7acf122fc8d88ee52c79ecc1b346b44f7b87
Ricardo Vale <ricardo_vale@live.com.pt>
2025-03-24 03:44:34 +0000

    Update tensor.hpp
    Signed-off-by: Ricardo Vale <ricardo_vale@live.com.pt>

--- cpp/include/axabsent/mathematics/tensor.hpp
+++ cpp/include/axabsent/mathematics/tensor.hpp
@@ -1,1 +1,29 @@
-Tensor operation headers
+// cpp/include/axabsent/mathematics/tensor.hpp
+
+#ifndef AXABSENT_MATHEMATICS_TENSOR_HPP
+#define AXABSENT_MATHEMATICS_TENSOR_HPP
+
+#include <Eigen/Dense>
+
+namespace axabsent {
+namespace mathematics {
+
+class TensorOps {
+public:
+    /// Compute the trace of a square tensor
+    static double trace(const Eigen::MatrixXd& T);
+
+    /// Normalize tensor to unit Frobenius norm
+    static Eigen::MatrixXd normalize(const Eigen::MatrixXd& T);
+
+    /// Compute the outer product: a ⊗ b
+    static Eigen::MatrixXd outer(const Eigen::VectorXd& a, const Eigen::VectorXd& b);
+
+    /// Compute double contraction: Tr(A * Bᵀ)
+    static double double_contraction(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B);
+};
+
+} // namespace mathematics
+} // namespace axabsent
+
+#endif // AXABSENT_MATHEMATICS_TENSOR_HPP
